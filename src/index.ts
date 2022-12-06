aa1efd5e21bb8242c062c986858ded08bb674767
idel28102001 <idel28102001@mail.ru>
2022-12-06 14:19:14 +0300

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import gpl from 'graphql-tag';
 
-
-
 export const animePage = gpl`
     query media($id: ID!,         
         $relatedFilter: FilterWithTypesInput,
@@ -109,8 +107,8 @@ export const animePage = gpl`
             authors(input: $authorsFilter) {
                 id
                 count
-                elements: {
-                    ...ProfileElement
+                elements {
+                    some
                 }
             }
             frames(input: $framesFilter) {
@@ -150,7 +148,7 @@ export const animePage = gpl`
             }
         }
     }
-    fragment Trailer on AnimePage {
+    fragment Trailer on AnimePage { 
         id
         url
         duration
@@ -165,4 +163,168 @@ export const animePage = gpl`
         url
     }
 `;
+
+// export const animePage = gpl`
+//     query media($id: ID!,         
+//         $relatedFilter: FilterWithTypesInput,
+//         $charactersFilter: FilterWithTypesInput,
+//         $authorsFilter: FilterInput,
+//         $framesFilter: FilterInput,
+//         $similarsFilter: FilterInput,
+//         ) {
+//         Media(input:$id) {
+//             id
+//             slug
+//             name
+//             originalName
+//             type
+//             episodesCount
+//             nextAiringEpisode {
+//                 airingAt
+//             }
+//             status
+//             startDate {
+//                 year
+//             }
+//             endDate {
+//                 year
+//             }
+//             genres {
+//                 id
+//                 slug
+//                 name
+//             }
+//             rating
+//             description
+//             bannerImage
+//             coverImage {
+//                 extraLarge
+//                 large
+//             }
+//             sections {
+//                 id
+//                 name
+//             }
+//             studioIcon {
+//                 url
+//                 name
+//             }
+//             trailer {
+//                 ...Trailer
+//             }
+//             related(input: $relatedFilter) {
+//                 id
+//                 title
+//                 count
+//                 types {
+//                     id
+//                     slug
+//                 }
+//                 elements {
+//                     id
+//                     title
+//                     releaseDate
+//                     type
+//                     image {
+//                         large {
+//                             ...Photo
+//                         }
+//                         extraLarge {
+//                             ...Photo
+//                         }
+//                     }
+//                 } 
+//             }
+//             characters(input: $charactersFilter) {
+//                 id
+//                 title
+//                 count
+//                 types {
+//                     id
+//                     slug
+//                 }
+//                 ...ProfileElement
+//             }
+//             characterPreview {
+//                 edge {
+//                     id
+//                     role
+//                     name {
+//                         userPreferred
+//                         original
+//                         english
+//                     }
+//                     description
+//                     voiceActor {
+//                         id
+//                         name {
+//                             userPreferred
+//                         }
+//                         role
+//                         language
+//                         image {
+//                             large
+//                         }
+//                     }
+//                 }
+//             }
+//             authors(input: $authorsFilter) {
+//                 id
+//                 count
+//                 elements: {
+//                     ...ProfileElement
+//                 }
+//             }
+//             frames(input: $framesFilter) {
+//                 trailer {
+//                     ...Trailer
+//                 }
+//                 coverImage {
+//                     large {
+//                         ...Photo
+//                     }
+//                     extraLarge {
+//                         ...Photo
+//                     }
+//                 }
+//             }
+//             similiars(input: $similarsFilter) {
+//                 id
+//                 image {
+//                     large {
+//                         ...Photo
+//                     }
+//                 }
+//                 title
+//                 episodesCount
+//                 releasedAt
+//                 type
+//             }
+//         }
+//     }
+
+//     fragment ProfileElement on AnimePage {
+//         id
+//         title
+//         image {
+//             large {
+//                 ...Photo
+//             }
+//         }
+//     }
+//     fragment Trailer on AnimePage { 
+//         id
+//         url
+//         duration
+//         frame {
+//             large {
+//                 ...Photo
+//             }
+//         }
+//     }
+//     fragment Photo on AnimePage {
+//         id
+//         url
+//     }
+// `;
 console.log(animePage);
